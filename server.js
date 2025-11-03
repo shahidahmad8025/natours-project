7392fdb718db99f840982126b64ed0272672ff35
Shahid Ahmad <shahidahmad8025@gmail.com>
2025-11-03 13:40:36 +0530

    server  file mod

--- server.js
+++ server.js
@@ -4,29 +4,15 @@ const dotenv = require('dotenv');
 // Only load dotenv in development
 if (process.env.NODE_ENV !== 'production') dotenv.config({ path: './config.env' });
 const app = require('./app');
-const DB = process.env.DATABASE.replace('<PASSWORD>', encodeURIComponent(process.env.DATABASE_PASSWORD));
-console.log(DB);
-
-// mongoose
-//   .connect(DB, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   })
-//   .then(() => console.log('DB connection successful!', DB));
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+console.log(process.env.NODE_ENV);
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
-  .then(() => {
-    console.log('DB connection successful!');
-  })
-  .catch((err) => {
-    console.log('DB connection error:', err);
-  });
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
