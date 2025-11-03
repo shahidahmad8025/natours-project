e9397f3575e1b6c25af2dadbb2f06f2593bbe225
Shahid Ahmad <shahidahmad8025@gmail.com>
2025-11-03 13:47:11 +0530

    server  file mod

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose');
-// const mongodb = require('mongodb');
 const dotenv = require('dotenv');
 // Only load dotenv in development
 if (process.env.NODE_ENV !== 'production') dotenv.config({ path: './config.env' });
 const app = require('./app');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-console.log(process.env.NODE_ENV);
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('DB connection successful!'));
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
