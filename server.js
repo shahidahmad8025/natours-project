25206621890404c33caa8a19dd4b8205e8ddf19b
Shahid Ahmad <shahidahmad8025@gmail.com>
2025-11-03 12:06:54 +0530

    SERVER  file mod

--- server.js
+++ server.js
@@ -7,14 +7,26 @@ const app = require('./app');
 const DB = process.env.DATABASE.replace('<PASSWORD>', encodeURIComponent(process.env.DATABASE_PASSWORD));
 console.log(DB);
 
+// mongoose
+//   .connect(DB, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log('DB connection successful!', DB));
+
 mongoose
   .connect(DB, {
-    useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connection successful!', DB));
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.log('DB connection error:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
